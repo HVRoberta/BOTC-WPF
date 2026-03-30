0667ea20b3803cf971cb9cda2774d6ab5b1e294c
DIRHABL1 <hvroberta@gmail.com>
2026-03-30 14:52:20 +0200

    Add shared controls for ActionBar, ErrorBanner, LoadingPanel, SectionCard, and StatusBadge; update CreateRoomView and JoinRoomView to utilize new components

---
+++ src/BOTC.Presentation.Desktop/Rooms/Shared/Controls/SectionCard.xaml.cs
@@ -0,0 +1,61 @@
+using System.Windows;
+using System.Windows.Controls;
+
+namespace BOTC.Presentation.Desktop.Rooms.Shared.Controls;
+
+public partial class SectionCard : UserControl
+{
+    public static readonly DependencyProperty TitleProperty = DependencyProperty.Register(
+        nameof(Title),
+        typeof(string),
+        typeof(SectionCard),
+        new PropertyMetadata(string.Empty));
+
+    public static readonly DependencyProperty SubtitleProperty = DependencyProperty.Register(
+        nameof(Subtitle),
+        typeof(string),
+        typeof(SectionCard),
+        new PropertyMetadata(string.Empty));
+
+    public static readonly DependencyProperty HeaderActionContentProperty = DependencyProperty.Register(
+        nameof(HeaderActionContent),
+        typeof(object),
+        typeof(SectionCard),
+        new PropertyMetadata(null));
+
+    public static readonly DependencyProperty BodyContentProperty = DependencyProperty.Register(
+        nameof(BodyContent),
+        typeof(object),
+        typeof(SectionCard),
+        new PropertyMetadata(null));
+
+    public SectionCard()
+    {
+        InitializeComponent();
+    }
+
+    public string Title
+    {
+        get => (string)GetValue(TitleProperty);
+        set => SetValue(TitleProperty, value);
+    }
+
+    public string Subtitle
+    {
+        get => (string)GetValue(SubtitleProperty);
+        set => SetValue(SubtitleProperty, value);
+    }
+
+    public object? HeaderActionContent
+    {
+        get => GetValue(HeaderActionContentProperty);
+        set => SetValue(HeaderActionContentProperty, value);
+    }
+
+    public object? BodyContent
+    {
+        get => GetValue(BodyContentProperty);
+        set => SetValue(BodyContentProperty, value);
+    }
+}
+
